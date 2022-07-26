2a993bbf1dc760909351e43c72ed6123819c1fd8
Samia Ouhadi <ouhadi@DomCIMR.MA>
2022-07-26 15:39:22 +0100

    h;jh

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
   
   login(username: string, password: string): Observable<any> {
     return this.http.post(
-      this.baseUrl + 'login',
+      this.baseUrl + 'signin',
       {username, password},
       httpOptions
     );
