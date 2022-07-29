9409a082b63793de091374954b327a0885424b0e
talhaoui ismail <talhaouiismail001@example.com>
2022-07-29 18:36:29 +0100

    start edit Temps de travail et RDV

--- src/app/pages/workinghours/workinghours.component.ts
+++ src/app/pages/workinghours/workinghours.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-workinghours',
@@ -8,13 +8,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class WorkinghoursComponent implements OnInit {
 
-  validateForm!: FormGroup;
+  validateForm: FormGroup;
 
   submitForm(): void {
     if (this.validateForm.valid) {
       console.log('submit', this.validateForm.value);
     } else {
-      Object.values(this.validateForm.controls).forEach(control  => {
+      console.log("non valide");
+      Object.values(this.validateForm.controls).forEach(
+        control  => {
         if (control.invalid) {
           control.markAsDirty();
           control.updateValueAndValidity({ onlySelf: true });
@@ -33,18 +35,12 @@ export class WorkinghoursComponent implements OnInit {
   monthFormat = 'yyyy/MM';
   
   ngOnInit(): void {
-    this.validateForm = this.fb.group({
-      effectiveTime: ['', [Validators.required]],
-      rdvTime: ['', [Validators.required]],
-      nombreConseillers: [''],
-      dateFrom: [null],
-      dateTo: [null],
-      datePicker: [null],
-      datePickerTime: [null],
-      monthPicker: [null],
-      rangePicker: [[]],
-      rangePickerTime: [[]],
-      timePicker: [null]
+    this.validateForm = new FormGroup({
+      effectiveTime: new FormControl('',[Validators.required]),
+      rdvTime: new FormControl('', [Validators.required]),
+      nombreConseillers:new FormControl( {value:'',disabled:true},[Validators.required]),
+      dateFrom: new FormControl('',[Validators.required]),
+      dateTo:new FormControl( '',[Validators.required])
     });
   }
 }
